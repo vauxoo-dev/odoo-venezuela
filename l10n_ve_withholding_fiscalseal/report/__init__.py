c00a2049a2bbe6e7d8548fdfcdbd28873969bdad
Humberto Arocha <hbto@vauxoo.com>
2015-07-13 01:52:43 -0500

    [IMP] Added a functional report for fiscal seal withholding

--- l10n_ve_withholding_fiscalseal/report/__init__.py
+++ l10n_ve_withholding_fiscalseal/report/__init__.py
@@ -2,8 +2,6 @@
 # -*- encoding: utf-8 -*-
 # http://www.gnu.org/licenses/agpl-3.0.html
 
-from . import model
-from . import wizard
-from . import report
+from . import parser
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ l10n_ve_withholding_fiscalseal/report/parser.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+# http://www.gnu.org/licenses/agpl-3.0.html
+
+import time
+
+from openerp.report import report_sxw
+from openerp.tools.translate import _
+
+
+class rep_comprobante(report_sxw.rml_parse):
+    # Variables Globales----------------------------------------------------
+    ttcompra = 0
+    ttcompra_sdcf = 0
+    ttretencion = 0
+    ttbase = 0
+    ttiva = 0
+
+    # ---------------------------------------------------------------------
+
+    def __init__(self, cr, uid, name, context):
+        super(rep_comprobante, self).__init__(cr, uid, name, context)
+        self.localcontext.update({
+            'time': time,
+            'get_partner_addr': self._get_partner_addr,
+        })
+
+    def _get_partner_addr(self, idp=False):
+        """ Return address2 partner
+        """
+        if not idp:
+            return []
+
+        addr_obj = self.pool.get('res.partner')
+        addr_inv = _('NO FISCAL ADDRESS DEFINED')
+        addr_inv = {}
+        if idp:
+            addr = addr_obj.browse(self.cr, self.uid, idp)
+            addr_inv = (
+                (addr.street and ('%s, ' % addr.street.title()) or '') +
+                (addr.zip and ('Codigo Postal: %s, ' % addr.zip) or '') +
+                (addr.city and ('%s, ' % addr.city.title()) or '') +
+                (addr.state_id and ('%s, ' % addr.state_id.name.title()) or '')
+                + (addr.country_id and ('%s ' % addr.country_id.name.title())
+                   or '') or _('NO INVOICE ADDRESS DEFINED'))
+            # addr_inv = (addr.street or '')+' '+(addr.street2 or '')+'
+            # '+(addr.zip or '')+ ' '+(addr.city or '')+ ' '+ (addr.country_id
+            # and addr.country_id.name or '')+ ', TELF.:'+(addr.phone or '')
+        return addr_inv
+
+
+report_sxw.report_sxw(
+    'report.account.wh.fiscalseal',
+    'account.wh.fiscalseal',
+    'addons/l10n_ve_withholding_fiscalseal/report/wh_fiscalseal_report.rml',
+    parser=rep_comprobante,
+    header=False
+)
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
